44eaf97a759d01e1e32d0a49d64453f20e65db34
midoks <midoks@163.com>
2016-09-12 19:15:17 +0800

    c

--- lua/lua_test/redis.lua
+++ lua/lua_test/redis.lua
@@ -3,7 +3,7 @@ local redis = require "resty.redis"
 local red = redis:new()
 local cjson = require "cjson"
 
-red:set_timeout(1000) -- 1s
+red:set_timeout(60000) -- 1s
 
 local ok, err = red:connect("127.0.0.1", 6379)
 
@@ -18,7 +18,7 @@ log_json["args"]=ngx.var.args
 log_json["host"]=ngx.var.host  
 log_json["request_body"]=ngx.var.request_body  
 log_json["remote_addr"] = ngx.var.remote_addr  
-log_json["remote_user"] = ngx.var.remote_user  
+log_json["remote_user"] = ngx.var.remote_user
 log_json["time_local"] = ngx.var.time_local  
 log_json["status"] = ngx.var.status  
 log_json["body_bytes_sent"] = ngx.var.body_bytes_sent  
@@ -31,9 +31,6 @@ log_json["test"] = "123123"
 
 local key = "logstash"
 local message = cjson.encode(log_json)
-red:lpush(key, message)
+red:rpush(key, message)
 
-
-ngx.header.content_type = "text/plain";
-
-ngx.say("1:","1")
+red:set_keepalive(100000, 1000)
