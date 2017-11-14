1f4cfee2ba4b1d83d947b09362eb98d1935f23f8
midoks <midoks@163.com>
2017-11-14 10:42:43 +0800

    solr 检查

---
+++ wiki/solr/solr_check.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+solr=` ps -ef|grep solr|grep -v "grep"| grep -v "solr_check.sh"`
+time=`date "+%Y-%m-%d %H:%M:%S"`
+
+if [ "$solr" = "" ];then
+    echo "$time" "running fail"
+    service solr restart
+else 
+    echo "$time" "running ok"
+fi 
+
