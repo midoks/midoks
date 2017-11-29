92da38fafb53f3ca41d46429cdaa26e1640b9c0f
midoks <midoks@163.com>
2017-11-29 19:55:05 +0800

    update

--- wiki/solr/solr_check.sh
+++ wiki/solr/solr_check.sh
@@ -5,7 +5,7 @@ time=`date "+%Y-%m-%d %H:%M:%S"`
 
 if [ "$solr" = "" ];then
     echo "$time" "running fail"
-    service solr restart
+    /sbin/service solr restart
 else 
     echo "$time" "running ok"
 fi 
