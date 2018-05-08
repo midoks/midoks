46a7c3452bfbe793fea6da0af4744cd310b57182
midoks <midoks@163.com>
2018-05-08 16:07:18 +0800

    update

--- golang/golex/php/i.php
+++ golang/golex/php/i.php
@@ -1,5 +1,24 @@
 <?php
 
+function t(){
+	return "12312";
+}
+
+var $t123 = t()(int);
+echo t();
+echo ++$t123;
 echo "123";
+
+echo 1<>2;
+
+$arr = [];
+$arr['tmp'] = "123456";
+$arr['i'] = 123000;
+
+var_dump($arr);
+
+$obj = new StdClass();
+var_dump($obj);
+exit('111');
 
 ?>
