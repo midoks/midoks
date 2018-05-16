4cbce3352feece22c0065b22bd1e8753d8251323
midoks <midoks@163.com>
2018-05-16 10:04:27 +0800

    php-mini

---
+++ golang/goyacc/php-mini/i.php
@@ -0,0 +1,36 @@
+<?php
+
+$b = 1+1;
+var_dump($b);
+
+// function t(){
+// 	return "12312";
+// }
+
+// var $t123 = t()(int);
+// echo t();
+// echo ++$t123;
+// echo "123";
+
+// echo 1<>2;
+
+// $arr = [];
+// $arr['tmp'] = "123456";
+// $arr['i'] = 123000;
+
+// var_dump($arr);
+
+// $obj = new StdClass();
+// var_dump($obj);
+// exit('111');
+
+
+/**
+ 12123123
+
+
+ 
+ */
+var_dump($b);
+
+?>
