0a79c2ecc61d603f8b75f10614d6f8ce9a837e0f
midoks <midoks@163.com>
2018-05-17 19:22:29 +0800

    js拷贝

---
+++ javascript/deepcp.js
@@ -0,0 +1,10 @@
+function deepCopy(obj){
+    if(typeof obj != 'object'){
+        return obj;
+    }
+    var newobj = {};
+    for ( var attr in obj) {
+        newobj[attr] = deepCopy(obj[attr]);
+    }
+    return newobj;
+}
