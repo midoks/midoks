5fcd4cfb9c1b0335bfbaabd43019f730fe435a7a
midoks <midoks@163.com>
2018-07-02 20:20:24 +0800

    up

--- php/common/common.php
+++ php/common/common.php
@@ -404,5 +404,26 @@ function userReplace($userName){
 }
 
 
+/**
+ * 根据起点坐标和终点坐标测距离
+ * @param  [array]   $from  [起点坐标(经纬度),例如:array(118.012951,36.810024)]
+ * @param  [array]   $to    [终点坐标(经纬度)]
+ * @param  [bool]    $km        是否以公里为单位 false:米 true:公里(千米)
+ * @param  [int]     $decimal   精度 保留小数位数
+ * @return [string]  距离数值
+ */
+function getDistance($from, $to, $km = true, $decimal=2){
+    sort($from);
+    sort($to);
+    $EARTH_RADIUS = 6370.996; // 地球半径系数
+    $distance = $EARTH_RADIUS*2*asin(sqrt(pow(sin( ($from[0]*pi()/180-$to[0]*pi()/180)/2),2)+cos($from[0]*pi()/180)*cos($to[0]*pi()/180)* pow(sin( ($from[1]*pi()/180-$to[1]*pi()/180)/2),2)))*1000;
+    
+    if($km){
+        $distance = $distance / 1000;
+    }
+    return round($distance, $decimal);
+}
+
+
 
 ?>
