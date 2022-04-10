8400c4dc4a19c3f28504e4ac43e904a4f4049357
midoks <midoks@163.com>
2022-04-11 00:18:50 +0800

    Create shell_prog_mem.sh

---
+++ shell/shell_prog_mem.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 获取内存数据，检查内存泄露
+# */1 * * * * ~/record/prog_mem.sh
+
+prog_name="imail"
+prog_mem=$(pidstat  -r -u -h -C $prog_name |awk 'NR==4{print $12}')
+time=$(date "+%Y-%m-%d %H:%M:%S")
+echo $time"\tmemory(Byte)\t"$prog_mem >>~/record/prog_mem.log
