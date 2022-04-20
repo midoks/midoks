3ab23ae05715aa5bb752638519b4c7535c5bf96d
midoks <midoks@163.com>
2022-04-20 20:20:42 +0800

    Update shell_prog_mem.sh

--- shell/shell_prog_mem.sh
+++ shell/shell_prog_mem.sh
@@ -3,6 +3,8 @@
 # 获取内存数据，检查内存泄露
 # */1 * * * * ~/record/prog_mem.sh
 
+# pidstat  -r -u -h -C gop2p |awk 'NR==4{print $12}'
+
 prog_name="imail"
 prog_mem=$(pidstat  -r -u -h -C $prog_name |awk 'NR==4{print $12}')
 time=$(date "+%Y-%m-%d %H:%M:%S")
