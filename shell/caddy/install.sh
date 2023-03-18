0ef2c8193d5fc04e20040a1e62bf47a12f0a9c86
midoks <midoks@163.com>
2023-03-18 23:02:14 +0800

    Update install.sh

--- shell/caddy/install.sh
+++ shell/caddy/install.sh
@@ -5,6 +5,7 @@ CADDY_FILE=https://github.com/caddyserver/caddy/releases/download/v2.6.4/caddy_2
 
 TMP_FILE=/tmp/caddy_2.6.4_linux_amd64.tar.gz
 if [ -f $TMP_FILE ];then
+	echo "wget --no-check-certificate -O $TMP_FILE $CADDY_FILE"
 	wget --no-check-certificate -O $TMP_FILE $CADDY_FILE
 fi
 
