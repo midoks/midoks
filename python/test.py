d90db8d1c289cbfd360bd7ebeca08c341e7d9b38
midoks <midoks@163.com>
2023-04-04 00:50:54 +0800

    Create test.py

---
+++ python/test.py
@@ -0,0 +1,30 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+import math
+
+
+def calc(base, p=0.05, t=1):
+
+    for x in range(t):
+        base = base * (1 + p)
+        print(x, base)
+    return base
+
+
+def calcV1(base, b=10000, p=0.05):
+    r = base * b
+
+    print(base, r)
+    for x in range(10000):
+        base = base * (1 + p)
+        if base > r:
+            break
+    return base, x
+
+
+# print(calc(7000, 0.05, 100))
+
+print(calcV1(7000, 3, 0.03))
+
+# print(math.pow(2, 10))
