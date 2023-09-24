84100fcee348acbb524cfb31f543eb32ffc7f14f
midoks <midoks@163.com>
2023-09-25 00:39:53 +0800

    Update sub_02.pl

--- perl/sub_02.pl
+++ perl/sub_02.pl
@@ -24,3 +24,46 @@ sub demo{
 
 $bammbamm += 1;
 print "$bammbamm\n";
+
+
+
+
+sub find{
+	my ($what, @array) = @_;
+	# $#array 获取最大下标
+	foreach(0..$#array){
+		if ($what eq $array[$_]){
+			return $_;
+		}
+	}
+	-1;
+}
+
+my @names = qw/ fred bar betty diao gosi /;
+my $result = &find("gosi",@names);
+
+print "find:$result\n";
+
+
+
+sub running_sum(){
+	state $sum = 0;
+	state @numbers;
+
+	# print "$sum\n";
+	# print "@numbers\n";
+
+
+	foreach my $number (@_) {
+		# print "$number\n";
+		push @numbers, $number;
+		$sum += $number;
+	}
+
+	say "the sum of (@numbers) is $sum";
+}
+
+
+&running_sum(5,6);
+&running_sum(1..3);
+&running_sum(4);
