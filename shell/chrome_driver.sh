3821a7d453021eecc8596be810f58e5055e47f1f
Mr Chen <midoks@163.com>
2023-11-23 15:24:27 +0800

    Create chrome_driver.sh

---
+++ shell/chrome_driver.sh
@@ -0,0 +1,6 @@
+apt install libnss3 -y
+apt install libnss3-dev -y
+apt install libasound2 -y
+apt install -y libatk1.0-0 libatk-bridge2.0-0 libcups2 libxkbcommon-x11-0 libxdamage1 libgbm-dev libpangocairo-1.0-0
+
+apt install alsa-lib-devel -y
