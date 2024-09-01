9dc53077427736592d41448e1440884fedadc9af
Mr Chen <midoks@163.com>
2024-09-01 17:54:43 +0800

    Update init.sh

--- html/h5_tpl/uniapp-vite-vue3-pinia-ts/init.sh
+++ html/h5_tpl/uniapp-vite-vue3-pinia-ts/init.sh
@@ -5,6 +5,7 @@ pnpm add @dcloudio/vite-plugin-uni
 pnpm add @esbuild/darwin-x64 -D 
 
 
+# fixbug: [ERROR] Cannot start service: Host version "0.16.17" does not match binary version "0.23.1"
 # pnpm add @esbuild/darwin-x64@0.16.17 -D 
 
 
