10cf5620f4b7fb569da600c0d423a43f6512805c
Mr Chen <midoks@163.com>
2024-09-01 22:07:48 +0800

    Update vite.config.js

--- html/h5_tpl/uni-preset-vue-vite-tabbar/vite.config.js
+++ html/h5_tpl/uni-preset-vue-vite-tabbar/vite.config.js
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
-import uni from '@dcloudio/vite-plugin-uni'
+import { defineConfig } from "vite";
+import { resolve } from 'path';
+import uni from "@dcloudio/vite-plugin-uni";
+import AutoImport from "unplugin-auto-import/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     uni(),
+    AutoImport({
+      imports: ["vue", "vue-router"],
+    }),
   ],
-})
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: resolve(__dirname, "src"), //配置@ 路径
+      },
+    ],
+  },
+});
