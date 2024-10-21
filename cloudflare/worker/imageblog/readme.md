0a86b4af5e0693633e04a2ba5a98b733520d695e
Mr Chen <midoks@163.com>
2024-10-21 22:23:54 +0800

    update

---
+++ cloudflare/worker/imageblog/readme.md
@@ -0,0 +1,9 @@
+
+### 常用cmd
+```
+pnpm install
+
+pnpm run dev
+
+
+```
--- cloudflare/worker/imageblog/src/index.js
+++ cloudflare/worker/imageblog/src/index.js
@@ -8,10 +8,55 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import url from 'url';
+import { Buffer } from 'node:buffer';
 
 export default {
     async fetch(request, env, ctx) {
         console.log("test");
+
+        const cache = caches.default;
+        const reqUrl = url.parse(request.url);
+
+        var image_url = reqUrl.pathname.substring(1);
+
+        if (image_url != ''){
+            if (image_url == "favicon.ico"){
+                return new Response('ok!');
+            }
+            console.log('fetch pre:'+image_url);
+            const buff = Buffer.from(image_url, 'base64');
+            image_url = buff.toString('utf-8');
+
+            console.log('fetch:'+image_url);
+            const cacheKey = new Request(image_url);
+
+            const hasCache = await cache.match(cacheKey);
+            if (hasCache) {
+                console.log('cache: hit');
+                return hasCache;
+            }
+
+            // 目标图片获取与检查
+            const imageRes = await fetch(image_url, { headers: request.headers });
+            if (!imageRes.ok) {
+                return imageRes;
+            }
+            console.log('fetch image done');
+            console.log(imageRes);
+            // const imageBytes = new Uint8Array(await imageRes.arrayBuffer());
+
+            // 返回体构造
+            const imageResponse = new Response(imageRes.body, {
+                headers: {
+                    'cache-control': 'public,max-age=86400',
+                },
+            });
+
+            // 写入缓存
+            ctx.waitUntil(cache.put(cacheKey, imageResponse.clone()));
+            return imageRes;
+        }
         return new Response('Hello World!');
     },
 };
