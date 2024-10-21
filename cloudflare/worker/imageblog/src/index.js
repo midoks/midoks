b75d2ede2bc0ffb05f3b4bbd2886fa6ba044d82d
Mr Chen <midoks@163.com>
2024-10-21 22:41:12 +0800

    Update index.js

--- cloudflare/worker/imageblog/src/index.js
+++ cloudflare/worker/imageblog/src/index.js
@@ -21,41 +21,47 @@ export default {
         var image_url = reqUrl.pathname.substring(1);
 
         if (image_url != ''){
-            if (image_url == "favicon.ico"){
-                return new Response('ok!');
-            }
-            console.log('fetch pre:'+image_url);
-            const buff = Buffer.from(image_url, 'base64');
-            image_url = buff.toString('utf-8');
+            try {
+                if (image_url == "favicon.ico"){
+                    return new Response('ok!');
+                }
+                console.log('fetch pre:'+image_url);
+                const buff = Buffer.from(image_url, 'base64');
+                image_url = buff.toString('utf-8');
 
-            console.log('fetch:'+image_url);
-            const cacheKey = new Request(image_url);
+                console.log('fetch:'+image_url);
+                const cacheKey = new Request(image_url);
 
-            const hasCache = await cache.match(cacheKey);
-            if (hasCache) {
-                console.log('cache: hit:'+cacheKey);
-                return hasCache;
-            }
+                const hasCache = await cache.match(cacheKey);
+                if (hasCache) {
+                    console.log('cache: hit:'+cacheKey);
+                    return hasCache;
+                }
 
-            // 目标图片获取与检查
-            const imageRes = await fetch(image_url, { headers: request.headers });
-            if (!imageRes.ok) {
+                // 目标图片获取与检查
+                const imageRes = await fetch(image_url, { headers: request.headers });
+                if (!imageRes.ok) {
+                    return imageRes;
+                }
+                console.log('fetch image done');
+                console.log(imageRes);
+                // const imageBytes = new Uint8Array(await imageRes.arrayBuffer());
+
+                // 返回体构造
+                const imageResponse = new Response(imageRes.body, {
+                    headers: {
+                        'cache-control': 'public,max-age=86400',
+                    },
+                });
+
+                // 写入缓存
+                ctx.waitUntil(cache.put(cacheKey, imageResponse.clone()));
                 return imageRes;
+
+            } catch (error) {
+                console.error('process:error', error.name, error.message, error);
+                return new Response('error!');
             }
-            console.log('fetch image done');
-            console.log(imageRes);
-            // const imageBytes = new Uint8Array(await imageRes.arrayBuffer());
-
-            // 返回体构造
-            const imageResponse = new Response(imageRes.body, {
-                headers: {
-                    'cache-control': 'public,max-age=86400',
-                },
-            });
-
-            // 写入缓存
-            ctx.waitUntil(cache.put(cacheKey, imageResponse.clone()));
-            return imageRes;
         }
         return new Response('Hello World!');
     },
