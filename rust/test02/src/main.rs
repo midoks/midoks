17f9d349ef76aff869e3db2835b71caef8fd4e0b
Mr Chen <midoks@163.com>
2024-12-06 02:44:35 +0800

    Update main.rs

--- rust/test02/src/main.rs
+++ rust/test02/src/main.rs
@@ -1,9 +1,19 @@
 
 
+#[repr(C)]
+struct A{
+    a: u8,
+    b: u32,
+    c: u16,
+}
 
 fn main() {
     let  raw_str = "EEEE don`t work here: \x3f \u{211D}";
 
     println!("Hello, {}!",raw_str);
     println!("Hello, world!");
+
+
+    println!("{:?}", std::mem::size_of::<A>()); // 8
+    let v = A{a:1,b:2,c:3};
 }
