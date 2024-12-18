4cddceaeef6ee2c63878874d975076fa43deb787
Mr Chen <midoks@163.com>
2024-12-18 23:32:38 +0800

    Update main.rs

--- rust/rrr/src/main.rs
+++ rust/rrr/src/main.rs
@@ -1,17 +1,25 @@
 use hotwatch::{Event, EventKind, Hotwatch};
 use std::{thread::sleep, time::Duration};
 
+use std::env;
+use std::path::Path;
+
 fn main() {
+    let cur_dir = env::current_dir();
+    println!("当前工作路径1:{:?}", cur_dir);
+    println!("当前工作路径2:{:?}", cur_dir.expect("不存在").display());
+
+    let mut path = cur_dir.expect("不存在").display();
+
+    println!("当前工作路径1:{:?}", path);
+
     let mut hotwatch = Hotwatch::new().expect("hotwatch failed to initialize!");
     hotwatch
-        .watch(
-            "/Users/midoks/Documents/GitHub/midoks/rust/one",
-            |event: Event| {
-                if let EventKind::Modify(_) = event.kind {
-                    println!("War has changed.");
-                }
-            },
-        )
+        .watch(path, |event: Event| {
+            if let EventKind::Modify(_) = event.kind {
+                println!("War has changed.");
+            }
+        })
         .expect("failed to watch file!");
 
     loop {
