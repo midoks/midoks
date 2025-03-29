13cf929c11ea3244e255bf0724c8df4fe4f0d1ad
dami <midoks@163.com>
2025-03-29 17:51:39 +0800

    Update run.sh

--- stock/one/run.sh
+++ stock/one/run.sh
@@ -16,6 +16,7 @@ BS_VENV(){
 
 BS_HELP(){
     echo "bash run.sh run|r            -> 策略1"
+    echo "bash run.sh r2               -> 下载最新数据并执行策略"
     echo "bash run.sh venv             -> 创建venv环境"
     echo "bash run.sh download|d       -> 下载数据"
     echo "bash run.sh version|v        -> 版本信息"
@@ -30,9 +31,15 @@ BS_CMD(){
     python3 analysis.py
 }
 
+BS_RUNTIME(){
+    python3 download.py
+    python3 analysis.py
+}
+
 
 case "$1" in
     "run" | "r") BS_CMD ;;
+    "r2") BS_RUNTIME ;;
     "download"| "d"  ) BS_DOWNLOAD ;;
     "version" | "v") BS_VERSION;;
     "venv") BS_VENV;;
