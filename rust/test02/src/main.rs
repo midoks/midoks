4ca432fb1915ae6f4a39a610b10a98209321cf47
dami <midoks@163.com>
2025-07-16 13:06:23 +0800

    Update main.rs

--- rust/test02/src/main.rs
+++ rust/test02/src/main.rs
@@ -1,19 +1,16 @@
-
-
 #[repr(C)]
-struct A{
+struct A {
     a: u8,
     b: u32,
     c: u16,
 }
 
 fn main() {
-    let  raw_str = "EEEE don`t work here: \x3f \u{211D}";
+    let raw_str = "EEEE don`t work here: \x3f \u{211D}";
 
-    println!("Hello, {}!",raw_str);
+    println!("Hello, {}!", raw_str);
     println!("Hello, world!");
 
-
     println!("{:?}", std::mem::size_of::<A>()); // 8
-    let v = A{a:1,b:2,c:3};
+    let _v = A { a: 1, b: 2, c: 3 };
 }
