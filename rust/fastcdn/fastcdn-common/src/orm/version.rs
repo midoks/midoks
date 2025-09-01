caec0871f66abdb920f298f9c609e11c73bacab0
dami <midoks@163.com>
2025-09-01 17:52:26 +0800

    Update version.rs

--- rust/fastcdn/fastcdn-common/src/orm/version.rs
+++ rust/fastcdn/fastcdn-common/src/orm/version.rs
@@ -9,7 +9,6 @@ pub async fn count() -> Result<i64, Box<dyn std::error::Error>> {
 // 更新记录版本信息
 pub async fn update(v: &str) -> Result<bool, Box<dyn std::error::Error>> {
     let db = pool::Manager::instance().await?;
-    println!("update:1");
     let num = count().await?;
     if num > 0 {
         let update = db
@@ -17,14 +16,9 @@ pub async fn update(v: &str) -> Result<bool, Box<dyn std::error::Error>> {
             .set_str("version", v)
             .where_id(1);
         let _affected = db.update_with_builder(update).await?;
-        println!("_affected:{:?}", _affected);
-        println!("update:1.1");
     } else {
-        println!("update:1.2");
         let insert = db.insert_builder("versions").set_str("version", v);
         let _id = db.insert_with_builder(insert).await?;
-        println!("_id:{:?}", _id);
     }
-    println!("update:2");
     Ok(true)
 }
