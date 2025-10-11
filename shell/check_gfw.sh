91d042fe47f755dcb6ffc43c40a8c56ac432ea99
dami <midoks@163.com>
2025-10-11 15:45:02 +0800

    Create check_gfw.sh

---
+++ shell/check_gfw.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# cmd
+# curl -fsSL https://raw.githubusercontent.com/midoks/midoks/refs/heads/master/shell/check_gfw.sh | bash
+
+find_gf=`ps -ef|grep 'xyjump/site' | grep -v grep`
+echo ${find_gf}
+if [ "${find_gf}" != ""  ];then
+    echo "site running"
+else
+	sh /etc/init.d/xyjump
+fi
+
+ps -ef|grep xyjump
+cat /usr/local/xyjump/port.txt
+
