91c7d933cadbbbe28f4e2ef2973719f899f87138
dami <midoks@163.com>
2025-10-11 15:57:48 +0800

    Update check_gfw.sh

--- shell/check_gfw.sh
+++ shell/check_gfw.sh
@@ -18,3 +18,15 @@ ps -ef|grep xyjump
 cat /usr/local/xyjump/port.txt
 cat /tmp/check_gfw.log
 
+
+find_node=`ps -ef|grep 'cloud-node' | grep -v grep`
+echo ${find_node}
+
+find_node=`ps -ef|grep 'cloud-node' | grep -v grep`
+if [ "${find_gf}" != ""  ];then
+    echo "cloud-node running"
+else
+	cloud-node restart
+    echo "cloud-node restart"
+fi
+
