2aa46b8a6d31c2ed4b670bc367a9a668ba7cf7dd
dami <midoks@163.com>
2025-10-11 16:46:28 +0800

    Update check_gfw.sh

--- shell/check_gfw.sh
+++ shell/check_gfw.sh
@@ -3,20 +3,22 @@
 # cmd
 # curl -fsSL https://raw.githubusercontent.com/midoks/midoks/refs/heads/master/shell/check_gfw.sh | bash
 
+run_time=`date "+%Y-%m-%d %H:%M:%S"`
 find_gf=`ps -ef|grep 'xyjump/site' | grep -v grep`
 echo ${find_gf}
 if [ "${find_gf}" != ""  ];then
     echo "site running"
-    echo "site running" > /tmp/check_gfw.log
+    echo "${run_time} site running" > /tmp/check_gfw.log
 else
 	sh /etc/init.d/xyjump
 	echo "site restart"
-	echo "site restart" > /tmp/check_gfw.log
+	echo "${run_time} site restart" > /tmp/check_gfw_restart.log
 fi
 
 ps -ef|grep xyjump
 cat /usr/local/xyjump/port.txt
 cat /tmp/check_gfw.log
+cat /tmp/check_gfw_restart.log
 
 
 find_node=`ps -ef|grep 'cloud-node' | grep -v grep`
