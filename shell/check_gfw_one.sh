3f505dbef7e84ee96600fa3bc344437ebf8ea2a7
dami <midoks@163.com>
2025-10-19 09:15:18 +0800

    Create check_gfw_one.sh

---
+++ shell/check_gfw_one.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# cmd
+# curl -fsSL https://raw.githubusercontent.com/midoks/midoks/refs/heads/master/shell/check_gfw_one.sh | bash
+TZ='Asia/Shanghai'
+run_time=$(TZ='Asia/Shanghai' date "+%Y-%m-%d %H:%M:%S")
+find_gf=`ps -ef|grep 'xyjump/site' | grep -v grep`
+echo ${find_gf}
+if [ "${find_gf}" != ""  ];then
+    echo "site running"
+    echo "${run_time} site running" > /tmp/check_gfw.log
+else
+	pkill site
+	sleep 3
+	sh /etc/init.d/xyjump
+	echo "site restart"
+	echo "${run_time} site restart" > /tmp/check_gfw_restart.log
+fi
+
+ps -ef|grep xyjump
+cat /usr/local/xyjump/port.txt
+cat /tmp/check_gfw.log
+
+if [ -f /tmp/check_gfw_restart.log ];then
+	cat /tmp/check_gfw_restart.log
+fi
+
+find_node=`ps -ef|grep 'cloud-node' | grep -v grep`
+echo ${find_node}
+if [ "${find_gf}" != ""  ];then
+    echo "cloud-node running"
+else
+	cloud-node restart
+    echo "cloud-node restart"
+fi
+
+find_nezha=`ps -ef|grep 'nezha-agent' | grep -v grep`
+echo ${find_nezha}
+if [ "${find_nezha}" != ""  ];then
+    echo "nezha-agent running"
+else
+	service nezha-agent restart
+    echo "nezha-agent restart"
+fi
+
+
+run_time_end=$(TZ='Asia/Shanghai' date "+%M")
+
+if [ "$run_time_end" == "30" ];then
+	echo "restart all start ${run_time_end}"
+	# cloud-node restart
+	pkill site
+	sleep 1
+	sh /etc/init.d/xyjump
+
+	echo "${run_time} auto restart" > /tmp/check_auto_restart.log
+	echo "restart all end"
+fi
+
+if [ -f /tmp/check_auto_restart.log ];then
+	cat /tmp/check_auto_restart.log
+fi
+
+
