364ac45ebd2b377d33aa86c9b1a5a9d600f30a3d
dami <midoks@163.com>
2025-10-19 09:19:48 +0800

    Update check_gfw_one.sh

--- shell/check_gfw_one.sh
+++ shell/check_gfw_one.sh
@@ -46,16 +46,16 @@ fi
 
 run_time_end=$(TZ='Asia/Shanghai' date "+%M")
 
-if [ "$run_time_end" == "30" ];then
-	echo "restart all start ${run_time_end}"
-	# cloud-node restart
-	pkill site
-	sleep 1
-	sh /etc/init.d/xyjump
+# if [ "$run_time_end" == "30" ];then
+echo "restart all start ${run_time_end}"
+# cloud-node restart
+pkill site
+sleep 1
+sh /etc/init.d/xyjump
 
-	echo "${run_time} auto restart" > /tmp/check_auto_restart.log
-	echo "restart all end"
-fi
+echo "${run_time} auto restart" > /tmp/check_auto_restart.log
+echo "restart all end"
+# fi
 
 if [ -f /tmp/check_auto_restart.log ];then
 	cat /tmp/check_auto_restart.log
