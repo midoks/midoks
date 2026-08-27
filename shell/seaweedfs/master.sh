957877447498fc5b6c85f1f74fbe2313b410e2bf
dami <midoks@163.com>
2026-08-27 15:46:42 +0800

    Update master.sh

--- shell/seaweedfs/master.sh
+++ shell/seaweedfs/master.sh
@@ -35,5 +35,7 @@ systemctl daemon-reload
 systemctl enable seaweedfs-master
 systemctl start seaweedfs-master
 
+# systemctl restart seaweedfs-master
+
 # systemctl status seaweedfs-master
 # journalctl -u seaweedfs-master -f
